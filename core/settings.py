90fe1d657c92a5ed40b9d77e230242aff4976832
nobiulhaque <mdriyad01624@gmail.com>
2026-04-11 14:47:47 +0600

    feat: enable Dashboard view at /dashboard/ endpoint

--- core/settings.py
+++ core/settings.py
@@ -47,7 +47,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'admin')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 from api import views as api_views
 
 from rest_framework_simplejwt.views import TokenRefreshView
@@ -18,6 +19,7 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/classify', api_views.ResumeClassifyAPIView.as_view(), name='resume_classify'),
     path('api/optimize', api_views.ResumeOptimizeAPIView.as_view(), name='resume_optimize'),
+    path('dashboard/', TemplateView.as_view(template_name='index.html'), name='dashboard'),
     path('api/', api_views.APIInfoAPIView.as_view(), name='api_info'),
     path('', api_views.APIInfoAPIView.as_view(), name='root'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
