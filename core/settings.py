3d2577dfdd68d2e4dd5a244b02d217d1ef66f6d7
nobiulhaque <mdriyad01624@gmail.com>
2026-04-11 14:46:37 +0600

    fix: include custom admin folder in static files for deployment

--- core/settings.py
+++ core/settings.py
@@ -74,6 +74,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'admin')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
