2befb6bd6569a0e3caed4f5f5bb4a5083ddb9d33
nobiulhaque <mdriyad01624@gmail.com>
2026-04-11 11:40:49 +0600

    feat: register Profile model and add custom delete action to User admin interface

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from django.utils.html import format_html
+from django.urls import reverse
 from .models import Profile
 
 @admin.register(Profile)
@@ -6,3 +10,22 @@ class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'location', 'profession', 'company_name')
     search_fields = ('user__username', 'user__email', 'location', 'profession')
     list_filter = ('profession',)
+
+# Customize User Admin to add a direct Delete button
+admin.site.unregister(User)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'delete_button')
+
+    def delete_button(self, obj):
+        # Only show for non-superusers or allow for all if owner desires
+        # Here we point to the confirmation page for deleting the user
+        url = reverse('admin:auth_user_delete', args=[obj.pk])
+        return format_html(
+            '<a href="{}" style="color: #ef4444; font-weight: bold; text-decoration: none;"><i class="fas fa-trash"></i> Delete</a>',
+            url
+        )
+    
+    delete_button.short_description = 'Actions'
+    delete_button.allow_tags = True
